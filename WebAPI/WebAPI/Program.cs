fb43392dc0f42db8d1dfb10faa548969466869b5
Chandramohan <Chandramohan.Kuntu@Chubb.com>
2023-08-13 22:01:01 +0530

    Initial

---
+++ WebAPI/WebAPI/Program.cs
@@ -0,0 +1,37 @@
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddHealthChecks().AddCheck<RandomHealthCheck>(nameof(RandomHealthCheck));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.MapHealthChecks("/healthz");
+            app.MapHealthChecks("/health/startup");
+            app.MapHealthChecks("/ready");
+          
+
+            
+            app.Run();
+        }
+    }
+}
